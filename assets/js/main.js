8a15771933687f3518b101406700e3948b3f1b1c
SuperCoder <supercode@MacBook-Air-von-SuperCode.fritz.box>
2022-01-30 17:46:43 +0100

    final at last???????

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let thanks = document.getElementById("thankMessage");
 
 function showName() {
     console.log("test");
-    thanks.innerHTML = 'Thank you, ' + first.value + ' ' + last.value + ', your are almost done. Please open your inbox and confirm the link provided. In case you cannot find our message please also check your junk mail. We are happy to keep you updated on all major events in ' + country.value + '.';
+    thanks.innerHTML = 'Thank you ' + first.value + ' ' + last.value + ', your are almost done. Please open your inbox and confirm the link provided. In case you cannot find our message please also check your junk mail. We are happy to keep you updated on all major events in ' + country.value + '.';
 }
 
 
