430ced9d8112b35fe0c6b313b459002d68099b09
SuperCoder <supercode@MacBook-Air-von-SuperCode.fritz.box>
2022-01-30 14:34:23 +0100

    position added

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,12 @@ let first = document.getElementById("firstName");
 console.log(first.value);
 let last = document.getElementById("lastName");
 console.log(last);
+let country = document.getElementById("country")
 let thanks = document.getElementById("thankMessage");
 
 function showName() {
     console.log("test");
-    thanks.innerHTML = 'Thank you, ' + first.value + ' ' + last.value + ', your are almost done. Please check your inbox for our email and confirm the link provided. In case you cannot find our message please also check your junk mail. Thank you!';
+    thanks.innerHTML = 'Thank you, ' + first.value + ' ' + last.value + ', your are almost done. Please open your inbox and confirm the link provided. In case you cannot find our message please also check your junk mail. We are happy to keep you updated on all major events in ' + country.value + '.';
 }
 
 
